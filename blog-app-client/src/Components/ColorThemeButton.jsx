153967bb95cedd4c0c9fbb9d8a9a81ce5e0fd89a
Auxence-M <auxencemedja@gmail.com>
2025-06-18 18:04:06 -0400

    Improved ColorThemeButton component

--- blog-app-client/src/Components/ColorThemeButton.jsx
+++ blog-app-client/src/Components/ColorThemeButton.jsx
@@ -1,72 +1,31 @@
 import {styled, useColorScheme} from '@mui/material/styles';
-import MenuItem from '@mui/material/MenuItem';
-import ContrastIcon from '@mui/icons-material/Contrast';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import {useState} from "react";
 import Box from "@mui/material/Box";
+import Tooltip from '@mui/material/Tooltip';
 
 const StyledIconButton = styled(IconButton)(({theme}) => ({
     color: theme.palette.primary.main,
 }));
 
 export default function ColorThemeButton() {
-    const[anchorElement, setAnchorElement] = useState();
-
     const {mode, setMode} = useColorScheme();
     if (!mode) {
         return null;
     }
 
-    function handleClose() {
-        setAnchorElement(null);
-    }
-
-    function handleClick(event) {
-        setAnchorElement(event.currentTarget);
-    }
-
-    function handleModeChange(targetMode) {
-        if (targetMode === "system") {
-            setMode("system");
-        } else if (targetMode === "light") {
-            setMode("light");
-        } else if (targetMode === "dark") {
-            setMode("dark");
-        }
-        setAnchorElement(null);
+    function handleClick() {
+       setMode(mode === "light" ? "dark" : "light");
     }
 
     return (
         <Box>
-            <StyledIconButton onClick={handleClick} size="medium" sx={{border: "1px solid", borderRadius: "0.5rem"}}>
-                {mode === "system" && <ContrastIcon />}
-                {mode === "light" && <LightModeOutlinedIcon />}
-                {mode === "dark" && <DarkModeOutlinedIcon />}
-            </StyledIconButton>
-
-            <Menu anchorEl={anchorElement} open={Boolean(anchorElement)} onClose={handleClose}>
-                <MenuItem sx={{paddingY: 1.5, borderRadius: "0.5rem"}}
-                          selected={mode === "system" }
-                          onClick={() => handleModeChange("system")}>
-                    <ContrastIcon sx={{marginRight: 1}}/>
-                    System
-                </MenuItem>
-                <MenuItem sx={{paddingY: 1.5, borderRadius: "0.5rem"}}
-                          selected={mode === "light"}
-                          onClick={() => handleModeChange("light")}>
-                    <LightModeOutlinedIcon sx={{marginRight: 1}} />
-                    Light
-                </MenuItem>
-                <MenuItem sx={{paddingY: 1.5, borderRadius: "0.5rem"}}
-                          selected={mode === "dark"}
-                          onClick={() => handleModeChange("dark")}>
-                    <DarkModeOutlinedIcon sx={{marginRight: 1}}/>
-                    Dark
-                </MenuItem>
-            </Menu>
+            <Tooltip title={mode === "light" ? "Turn off the light" : "Turn on the light"} arrow>
+                <StyledIconButton onClick={handleClick} size="small" sx={{border: "1px solid", borderRadius: "0.5rem"}}>
+                    {mode === "dark" ? <LightModeOutlinedIcon /> : <DarkModeOutlinedIcon/>}
+                </StyledIconButton>
+            </Tooltip>
         </Box>
     );
 }
