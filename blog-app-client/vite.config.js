f31d50febd27f361f8850744efa7c1a2bf91064d
Auxence-M <auxencemedja@gmail.com>
2025-07-02 11:10:05 -0400

    added docker-compose file

--- blog-app-client/vite.config.js
+++ blog-app-client/vite.config.js
@@ -5,10 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 5173,
+    watch: {
+      usePolling: true,
+    },
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://blog-app-api:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
